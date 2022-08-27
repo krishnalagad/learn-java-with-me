d07b5a82b4cc9e470c7a4729688fe7d34b81b94b
Krishna Lagad <krishnalagad2@gmail.com>
2022-08-27 22:22:31 +0530

    Update CompareNumbers.java

---
+++ Java String Methods/CompareNumbers.java
@@ -0,0 +1,10 @@
+class CompareNumbers{
+    public static int compareNumber(Integer no1, Integer no2) {
+
+        return no1.compareTo(no2);
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
