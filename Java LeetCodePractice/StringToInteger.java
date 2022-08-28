547a6516c0a89d6843fd4fce58f35cd980c0a133
Krishna Lagad <krishnalagad2@gmail.com>
2022-08-28 14:57:20 +0530

    Update StringToInteger.java
    Runtime enhanced to 7ms.

--- Java LeetCodePractice/StringToInteger.java
+++ Java LeetCodePractice/StringToInteger.java
@@ -1,30 +1,28 @@
 public class StringToInteger {
     public static int myAtoi(String s) {
 
-        s = s.trim(); // trimming white space
+        s = s.trim(); 
         long ans = 0;
         if (s.equals(""))
-            return 0; // if string is empty return 0
+            return 0; 
         int neg = 1;
-        if (s.charAt(0) == '+') { // check if first char is + then d nothing
+        if (s.charAt(0) == '+') { 
 
-        } else if (s.charAt(0) == '-') { // check if first char is - then number is negative
+        } else if (s.charAt(0) == '-') { 
             neg = -1;
-        } else if (Character.isDigit(s.charAt(0))) { // if first vharcter is numeric add to ans
+        } else if (Character.isDigit(s.charAt(0))) { 
             ans = Character.getNumericValue(s.charAt(0));
             System.out.println(ans);
-        } else { // if first charcter is other than (+ , - , or digit) reurn 0
+        } else { 
             return 0;
         }
 
-        for (int i = 1; i < s.length(); i++) { // iterate to left of string means from index 1
-
-            if (Character.isDigit(s.charAt(i))) { // if it is numeric then add into ans;
+        for (int i = 1; i < s.length(); i++) {
+            if (Character.isDigit(s.charAt(i))) { 
                 ans = ans * 10;
                 ans = ans + Character.getNumericValue(s.charAt(i));
 
-                if (neg * ans < Integer.MIN_VALUE) { // at any case over ans get out of bound for integer than clamp it
-                                                     // and return
+                if (neg * ans < Integer.MIN_VALUE) { 
                     return Integer.MIN_VALUE;
                 }
                 if (neg * ans > Integer.MAX_VALUE) {
@@ -36,7 +34,7 @@ public class StringToInteger {
 
         }
 
-        return neg * (int) ans; // at last return ans
+        return neg * (int) ans; 
 
     }
 
