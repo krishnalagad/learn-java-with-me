e797a438546a46b6aa2a0f93c68f31ec13e483a9
Krishna Lagad <krishnalagad2@gmail.com>
2022-08-28 12:53:19 +0530

    Update AddValuesOfTwoLinkedList.java

--- Java LeetCodePractice/AddValuesOfTwoLinkedList.java
+++ Java LeetCodePractice/AddValuesOfTwoLinkedList.java
@@ -19,31 +19,33 @@ class Solution {
     }
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode head = new ListNode(l1.val + l2.val);
+        ListNode head = new ListNode(0);
         ListNode rawNode = head;
         int carry = 0;
 
         while (l1 != null || l2 != null || carry != 0) {
-            int sumOfL1 = 0;
-            int sumOfL2 = 0;
+            int valOfL1 = 0;
+            int valOfL2 = 0;
             int finalSum = 0;
 
             // for linked list 1
-            if (l1 != null)
-                sumOfL1 = l1.val;
-            else
-                sumOfL1 = 0;
+            if (l1 != null) {
+                valOfL1 = l1.val;
+                // l1 = l1.next;
+            } else
+                valOfL1 = 0;
 
             // for linked list 2
-            if (l2 != null)
-                sumOfL2 = l2.val;
-            else
-                sumOfL1 = 0;
+            if (l2 != null) {
+                valOfL2 = l2.val;
+                // l2 = l2.next;
+            } else
+                valOfL2 = 0;
 
-            finalSum = sumOfL1 + sumOfL2 + carry;
+            finalSum = valOfL1 + valOfL2 + carry;
             carry = finalSum / 10; // to get the carry divide sum by 10.
-            rawNode = new ListNode(finalSum % 10);
-            head.next = rawNode;
+            rawNode.next = new ListNode(finalSum % 10);
+            // head.next = rawNode;
             rawNode = rawNode.next;
 
             if (l1 != null)
@@ -52,6 +54,6 @@ class Solution {
                 l2 = l2.next;
         }
 
-        return head;
+        return head.next;
     }
 }
