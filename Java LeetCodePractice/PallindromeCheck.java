dc150ea5f2ba557c46bb29854c8861eff7fea675
Krishna Lagad <krishnalagad2@gmail.com>
2022-08-28 15:10:28 +0530

    Update PallindromeCheck.java
    Runtime enhanced to 0 ms.

--- Java LeetCodePractice/PallindromeCheck.java
+++ Java LeetCodePractice/PallindromeCheck.java
@@ -1,22 +1,32 @@
 public class PallindromeCheck {
-    public static boolean isPalindrome(int x) {
-        int num = 0;
+    // public static boolean isPalindrome(int x) {
+    //     int num = 0;
 
+    //     if (x < 0)
+    //         return false;
+
+    //     while (x != 0) {
+    //         int rem = x % 10;
+    //         num = num * 10 + rem;
+    //         x = x / 10;
+    //     }
+
+    //     if (x == num)
+    //         return true;
+    //     else
+    //         return false;
+    // }
+    public static boolean isPalindrome(int x) {
         if (x < 0)
             return false;
 
-        while (x != 0) {
-            int rem = x % 10;
-            num = num * 10 + rem;
-            x = x / 10;
-        }
-
-        if (x == num)
-            return true;
-        else
-            return false;
+        int raw = x;
+        Integer a = Integer.valueOf(x);
+        StringBuffer buffer = new StringBuffer(String.valueOf(raw));
+        boolean flag = (a.compareTo(Integer.valueOf(String.valueOf(buffer.reverse()))) == 0) ? true : false;
+        return flag;
     }
     public static void main(String[] args) {
-        System.out.println(isPalindrome(121));
+        System.out.println(isPalindrome(123));
     }
 }
