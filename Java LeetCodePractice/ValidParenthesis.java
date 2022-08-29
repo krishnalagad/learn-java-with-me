1854629f28a444f2946a6b2b1656793f735813b5
Krishna Lagad <krishnalagad2@gmail.com>
2022-08-29 19:17:29 +0530

    Create ValidParenthesis.java
    - Time complexity:
         - O(n) : 'n' is the length of given string.

---
+++ Java LeetCodePractice/ValidParenthesis.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class ValidParenthesis {
+    public static boolean isValid(String s) {
+        Deque<Character> st = new ArrayDeque<>();
+        for (char ch : s.toCharArray()) {
+            if (ch == '(') {
+                st.addFirst(')');
+            } else if (ch == '{') {
+                st.addFirst('}');
+            } else if (ch == '[') {
+                st.addFirst(']');
+            } else if (!st.isEmpty() && st.peek() == ch) {
+                st.removeFirst();
+            } else {
+                return false;
+            }
+        }
+        if (!st.isEmpty()) {
+            return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        System.out.println(isValid("()[]{}"));
+    }
+}
