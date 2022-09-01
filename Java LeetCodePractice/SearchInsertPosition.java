92d873ff709a3ea5d6f8d8b6455f0cc4729d40e5
Krishna Lagad <krishnalagad2@gmail.com>
2022-09-01 20:19:53 +0530

    Update SearchInsertPosition.java
    - Working on:
         if element doesn't exists in the input array and less than greatest element in array.

--- Java LeetCodePractice/SearchInsertPosition.java
+++ Java LeetCodePractice/SearchInsertPosition.java
@@ -6,18 +6,24 @@ public class SearchInsertPosition {
         List<Integer> list = Arrays.stream(nums).boxed().collect(Collectors.toList());
 
         // 1. if element exists in the input array i.e nums
-        if(list.indexOf(target) != -1){
+        if (list.indexOf(target) != -1) {
             return list.indexOf(target);
         }
 
-        // 2. if element doesn't exists in the input array i.e nums
-        
+        // 2. if element doesn't exists in the input array and less than greatest
+        // element in array
+        for (int i : list) {
+            if (i > target) {
+                return list.indexOf(i);
+            }
+        }
+
 
         return 0;
     }
 
     public static void main(String[] args) {
         int[] arr = { 1, 3, 5, 6 };
-        System.out.println(searchInsert(arr, 5));
+        System.out.println(searchInsert(arr, 7));
     }
 }
