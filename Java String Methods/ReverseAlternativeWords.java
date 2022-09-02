200ec04190e5ba50701b2e6c97493371736e1e86
Krishna Lagad <krishnalagad2@gmail.com>
2022-09-02 21:12:26 +0530

    Create ReverseAlternativeWords.java

---
+++ Java String Methods/ReverseAlternativeWords.java
@@ -0,0 +1,18 @@
+public class ReverseAlternativeWords {
+    public static void printAlternativeReverse(String str){
+        String[] strArr = str.split(" ");
+        String result = "";
+        for (int i = 0; i < strArr.length; i++) {
+            if(i % 2 != 0){
+                StringBuffer sb = new StringBuffer(strArr[i]);
+                result += String.valueOf(sb.reverse()) + " ";
+            }else{
+                result += String.valueOf(strArr[i]) + " ";
+            }
+        }
+        System.out.println(result);
+    }
+    public static void main(String[] args) {
+        printAlternativeReverse("my name is krishna dilip lagad");
+    }
+}
