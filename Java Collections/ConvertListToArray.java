75b91e9058537783025ab640c017063642cec1cb
Krishna Lagad <krishnalagad2@gmail.com>
2022-09-02 11:16:51 +0530

    Create ConvertListToArray.java

---
+++ Java Collections/ConvertListToArray.java
@@ -0,0 +1,30 @@
+import java.util.LinkedList;
+
+public class ConvertListToArray {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<Integer>();
+
+        list.add(40);
+        list.add(30);
+        list.add(50);
+        list.add(80);
+        list.add(90);
+        list.add(10);
+
+        for (Integer i : list) {
+            System.out.print("ELements in list : " + i);
+        }
+
+        int arr[] = new int[list.size()];
+        int arr1[] = new int[list.size()];
+        arr = list.stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
+
+        Object[] res = list.toArray();
+        System.out.println("Printing elements of array");
+        for (int value : arr1) {
+            System.out.print(value + " ");
+        }
+    }
+}
