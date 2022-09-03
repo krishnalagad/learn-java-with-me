54c2be5770e4cd22c382ab83fb0e1d2c155e3bcc
Krishna Lagad <krishnalagad2@gmail.com>
2022-09-03 22:31:05 +0530

    Update PerferctSquare.java

--- Java LeetCodePractice/PerferctSquare.java
+++ Java LeetCodePractice/PerferctSquare.java
@@ -1,38 +1,40 @@
 import java.util.*;
 
 public class PerferctSquare {
+    static List<Integer> list1 = new ArrayList<Integer>();
+    static int a = 1;
 
     public static int solve(int n) {
         double root = Math.sqrt(n);
+        list1.add(a);
+        list1.set(0, a);
+        a += 1;
         System.out.println(root);
         int countPos = 0;
         int countNeg = 0;
-        List<Integer> list1 = new ArrayList<Integer>();
 
         if (n % root == 0 && !list1.isEmpty())
             return list1.get(0);
-        
+
         if (n % root == 0)
             return countNeg;
 
         String str = String.valueOf(root);
         if (str.indexOf(".") != -1) {
-            // System.out.println(str);
-            for (int i = 0; i < Integer.MAX_VALUE - 1; i++) {
+            for (int i = 0; i < Integer.MAX_VALUE - 1;) {
                 countNeg++;
-                list1.add(i+1);
-                list1.set(0, i+1);
+                list1.add(i + 1);
+                list1.set(0, i + 1);
                 n -= 1;
                 solve(n);
-                
-                if (n % root == 0)
-                    return list1.get(0);
+
+                return list1.get(0) - 1;
             }
         }
         return list1.get(0);
     }
 
     public static void main(String[] args) {
-        System.out.println(solve(28));
+        System.out.println(solve(45));
     }
 }
