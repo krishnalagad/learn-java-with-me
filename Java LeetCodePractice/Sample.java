e15a364ec21e1d6a0a26ff3e395d1c39d95419e1
Krishna Lagad <krishnalagad2@gmail.com>
2022-09-03 16:05:44 +0530

    Create Sample.java

---
+++ Java LeetCodePractice/Sample.java
@@ -0,0 +1,12 @@
+public class Sample {
+    /**
+     * @param n : number of candles and it is also the price of one candle.
+     * @return : price of all candles ravi has bought from shopkeeper
+     */
+    public static int solve(int n){
+        return n * n;
+    }
+    public static void main(String[] args) {
+        System.out.println(solve(4));
+    }
+}
