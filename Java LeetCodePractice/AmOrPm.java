cf618c9cfb6668290c3b75b878d1e89b0c02197a
Krishna Lagad <krishnalagad2@gmail.com>
2022-09-03 15:52:10 +0530

    Create AmOrPm.java

---
+++ Java LeetCodePractice/AmOrPm.java
@@ -0,0 +1,17 @@
+public class AmOrPm {
+
+    public static String solve(String time){
+        String[] strArr = time.split(":");
+        int hours = Integer.valueOf(strArr[0]);
+        if(hours > 12){
+            return "PM";
+        }
+        return "AM";
+    }
+    public static void main(String[] args) {
+        System.out.println(solve("11:45"));
+        System.out.println(solve("00:45"));
+        System.out.println(solve("02:45"));
+        System.out.println(solve("15:45"));
+    }
+}
