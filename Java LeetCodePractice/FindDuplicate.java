d81afda8356d537ff3e0725e024088a26e57ce33
Krishna Lagad <krishnalagad2@gmail.com>
2022-09-05 20:59:23 +0530

    Create FindDuplicate.java

---
+++ Java LeetCodePractice/FindDuplicate.java
@@ -0,0 +1,10 @@
+public class FindDuplicate {
+    public static int findDuplicate(int[] nums) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 7, 8, 9, 11, 12, 9 };
+        System.out.println(findDuplicate(nums));
+    }
+}
