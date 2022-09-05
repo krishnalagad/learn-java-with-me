fd9f8c230ed3c92365633cbaaf48714937743f33
Krishna Lagad <krishnalagad2@gmail.com>
2022-09-05 23:13:26 +0530

    Update ReverseSentence.java
    Optimizing code

--- Java String Methods/ReverseSentence.java
+++ Java String Methods/ReverseSentence.java
@@ -1,11 +1,17 @@
+import java.util.*;
+
 public class ReverseSentence {
     public static void reverseSentence(String str){
         String[] raw = str.split(" ");
         String result = "";
-        for (int i = raw.length; i > 0; i--) {
+        List<String> list = new ArrayList<String>();
+        for (int i = raw.length - 1; i >= 0; i--) {
             result = result +  raw[i] + " ";
+            list.add(raw[i]);
         }
         System.out.println(result);
+        System.out.println(String.join(" ", list));
+        System.out.println(result.length() + "  " + String.join(" ", list).length());
     }
 
     public static void main(String[] args) {
