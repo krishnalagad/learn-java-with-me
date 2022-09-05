3018ea79025e07a8323c0000b1a2bbc7748749c1
Krishna Lagad <krishnalagad2@gmail.com>
2022-09-05 21:07:35 +0530

    Update FindDuplicate.java

--- Java LeetCodePractice/FindDuplicate.java
+++ Java LeetCodePractice/FindDuplicate.java
@@ -1,6 +1,23 @@
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class FindDuplicate {
     public static int findDuplicate(int[] nums) {
-        return 0;
+        return Arrays
+                .stream(nums)
+                .boxed()
+                .collect(
+                        Collectors.groupingBy(
+                                Function.identity(),
+                                Collectors.counting()))
+                .entrySet()
+                .stream()
+                .filter(m -> m.getValue() > 1)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList())
+                .get(0);
     }
 
     public static void main(String[] args) {
