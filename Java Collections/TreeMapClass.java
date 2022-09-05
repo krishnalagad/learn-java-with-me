21122a65b5510c01d69873214c6ef477f22e26fd
Krishna Lagad <krishnalagad2@gmail.com>
2022-09-06 00:22:48 +0530

    Create TreeMapClass.java

---
+++ Java Collections/TreeMapClass.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class TreeMapClass {
+    public static int solve(Map<?,?> map){
+        Set<Integer> keys = new LinkedHashSet<Integer>();
+        
+        return 0;
+    }
+    public static void main(String[] args) {
+        Map<Integer, String> tMap = new TreeMap<Integer, String>(); 
+
+        tMap.put(20, "Green");
+        tMap.put(10, "Red");
+        tMap.put(40, "Black");
+        tMap.put(50, "White");
+        tMap.put(60, "Pink");
+
+        System.out.println(tMap);
+        // System.out.println(tMap.lowerKey);
+    }
+}
