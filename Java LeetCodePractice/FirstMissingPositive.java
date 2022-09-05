47e3a74d3dfae71f933379a573e36dad743ef025
Krishna Lagad <krishnalagad2@gmail.com>
2022-09-05 20:53:54 +0530

    Update FirstMissingPositive.java

--- Java LeetCodePractice/FirstMissingPositive.java
+++ Java LeetCodePractice/FirstMissingPositive.java
@@ -25,7 +25,6 @@ public class FirstMissingPositive {
         return n + 1;
 
     }
-
     public static void main(String[] args) {
         int[] nums = { 7, 8, 9, 11, 12};
         System.out.println(firstMissingPositive(nums));
