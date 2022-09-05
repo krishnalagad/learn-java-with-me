7dd69c42ff612b00212ff94f5dc1845899539aac
Krishna Lagad <krishnalagad2@gmail.com>
2022-09-05 21:28:30 +0530

    Update FindDuplicate.java

--- Java LeetCodePractice/FindDuplicate.java
+++ Java LeetCodePractice/FindDuplicate.java
@@ -21,15 +21,15 @@ public class FindDuplicate {
     // }
 
     // 2. Approach 2
-    public static int findDuplicate(int[] nums) {
-        Set<Integer> items = new HashSet<>();
-        return Arrays
-                    .stream(nums)
-                    .boxed()
-                    .filter(n -> !items.add(n))
-                    .collect(Collectors.toList())
-                    .get(0);
-    }
+    // public static int findDuplicate(int[] nums) {
+    //     Set<Integer> items = new HashSet<>();
+    //     return Arrays
+    //                 .stream(nums)
+    //                 .boxed()
+    //                 .filter(n -> !items.add(n))
+    //                 .collect(Collectors.toList())
+    //                 .get(0);
+    // }
 
     // 3. Approach 3
     // public static int findDuplicate(int[] nums) {
@@ -40,6 +40,18 @@ public class FindDuplicate {
     //             .collect(Collectors.toList())
     //             .get(0);
     // }
+
+    // 4. Approach 4
+    public static int findDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int a : nums) {
+            if (set.contains(a)) {
+                return a;
+            }
+            set.add(a);
+        }
+        return -1;
+    }
 
     public static void main(String[] args) {
         int[] nums = { 7, 8, 9, 11, 12, 9 };
