a00bf7ab3893baf719551256774bdd8a7d55f77e
Krishna Lagad <krishnalagad2@gmail.com>
2022-09-10 23:18:26 +0530

    Create RepeatedElement.java

---
+++ Java LeetCodePractice/RepeatedElement.java
@@ -0,0 +1,14 @@
+public class RepeatedElement {
+    public static int findSpecialInteger(int[] arr) {
+
+
+
+        
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 2, 6, 6, 6, 6, 7, 10 };
+        System.out.println(findSpecialInteger(arr));
+    }
+}
