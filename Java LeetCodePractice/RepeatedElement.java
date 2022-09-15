305fe8c3e3687b4e854a38c2ebd9f9ef8b8d353a
Krishna Lagad <krishnalagad2@gmail.com>
2022-09-15 15:36:27 +0530

    Update RepeatedElement.java

--- Java LeetCodePractice/RepeatedElement.java
+++ Java LeetCodePractice/RepeatedElement.java
@@ -1,11 +1,58 @@
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
 public class RepeatedElement {
-    public static int findSpecialInteger(int[] arr) {
-        
-        return 0;
+
+    public static String rec(String string) {
+        if (string.length() <= 1) {
+            return string; //
+
+        }
+        String str1 = string.substring(0, 1);
+        String str2 = string.substring(1);
+        return str1 + rec(str2);
+
     }
 
+    public void example(Predicate<Integer> func, ArrayList<Integer> items) {
+        int i = 0;
+        for (int item : items) {
+            if (func.test(item)) {
+                items.set(i, item);
+                i += 1;
+            }
+        }
+        items.subList(i, items.size()).clear();
+        ;
+    }
+
+    public static void func(int a, List<Integer> b){
+        a += 1;
+        b.add(1);
+    }
+
+    public static boolean foo(String str1, String str2){
+        if(str1.length() != str2.length()) return false;
+
+        for (int i = 0; i < str1.length(); i++) {
+            if(str1.charAt(str1.length() -i - 1 ) == str2.charAt(i) ){
+                continue;
+            }else{
+                return str1.charAt(str1.length() - i - 1) < str2.charAt(i);
+            }
+        }
+        return true;
+    }
     public static void main(String[] args) {
-        int[] arr = { 1, 2, 2, 6, 6, 6, 6, 7, 10 };
-        System.out.println(findSpecialInteger(arr));
+        System.out.println(rec("krishna"));
+
+        int a = 0;
+        List<Integer> b = new ArrayList<Integer>();
+        func(a, b);
+        System.out.println(a + b.toString());
+
+        System.out.println(foo("818", "718"));
+
     }
 }
