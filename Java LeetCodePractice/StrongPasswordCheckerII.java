0d446485eb216052b0d824d588c232b5a0c5f9ad
Krishna Lagad <krishnalagad2@gmail.com>
2022-09-17 23:32:13 +0530

    Update StrongPasswordCheckerII.java

--- Java LeetCodePractice/StrongPasswordCheckerII.java
+++ Java LeetCodePractice/StrongPasswordCheckerII.java
@@ -1,8 +1,25 @@
 public class StrongPasswordCheckerII {
+    public static boolean checkDuplicateAtAdjecent(String s) {
+        char[] chars = s.toCharArray();
+        char prev = 0;
+        int k = 0;
+        for (char c : chars) {
+            if (prev != c) {
+                chars[k++] = c;
+                prev = c;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static boolean strongPasswordCheckerII(String password) {
         boolean digitFlag = false;
         boolean upperFlag = false;
         boolean lowerFlag = false;
+        boolean sameFlag = false;
+        boolean specFlag = false;
         if (password == null || password.length() < 8)
             return false;
 
@@ -16,11 +33,18 @@ public class StrongPasswordCheckerII {
             if (Character.isLowerCase(password.charAt(i))) {
                 lowerFlag = true;
             }
+            sameFlag = checkDuplicateAtAdjecent(password);
+            if (!Character.isDigit(password.charAt(i))
+                    && !Character.isLetter(password.charAt(i))
+                    && !Character.isWhitespace(password.charAt(i))) {
+
+                specFlag = true;
+            }
         }
-        return true;
+        return digitFlag && lowerFlag && upperFlag && specFlag && sameFlag;
     }
 
     public static void main(String[] args) {
-        System.out.println(strongPasswordCheckerII("Krishna"));
+        System.out.println(strongPasswordCheckerII("Krishna9@"));
     }
 }
