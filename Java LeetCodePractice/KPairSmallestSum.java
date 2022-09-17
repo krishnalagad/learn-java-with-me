0328e197ba900017c3f739ad9b4b34bc1ed9b063
Krishna Lagad <krishnalagad2@gmail.com>
2022-09-17 22:04:41 +0530

    Update KPairSmallestSum.java
    - used stream api to get first n elements from the list.

--- Java LeetCodePractice/KPairSmallestSum.java
+++ Java LeetCodePractice/KPairSmallestSum.java
@@ -1,24 +1,27 @@
 import java.util.*;
+import java.util.stream.*;
 
 public class KPairSmallestSum {
     public static List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
         List<List<Integer>> ans = new ArrayList<>();
-        for (int i = 0; i < nums1.length; i++) {   
+        for (int i = 0; i < nums1.length; i++) {
             for (int j = 0; j < nums2.length; j++) {
                 List<Integer> list = new ArrayList<Integer>();
                 list.add(nums1[i]);
                 list.add(nums2[j]);
                 ans.add(list);
-                System.out.println(ans);
             }
-            
         }
-        return ans.subList(0, k);
+        // Approach 1 : to get first n elements from list.
+        // return ans.subList(0, k);
+
+        // Approach 2 : to get first n elements from list using stream.
+        return ans.stream().limit(k).collect(Collectors.toList());
     }
 
     public static void main(String[] args) {
-        int[] arr1 = { 1, 7, 11 };
-        int[] arr2 = { 2, 4, 6 };
+        int[] arr1 = { 1, 1, 2 };
+        int[] arr2 = { 1, 2, 3 };
         System.out.println(kSmallestPairs(arr1, arr2, 3));
     }
 }
