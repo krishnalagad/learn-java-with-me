2645ded4efc7e9c8847d75ee1bcd9a5fc27ff91d
Krishna Lagad <krishnalagad2@gmail.com>
2022-09-18 01:12:09 +0530

    Create ConsecutiveSequenceWithSum.java

---
+++ Java LeetCodePractice/ConsecutiveSequenceWithSum.java
@@ -0,0 +1,27 @@
+public class ConsecutiveSequenceWithSum {
+    static void findConsecutive(int N) {
+        int start = 1;
+        int end = (N + 1) / 2;
+
+        while (start < end) {
+            int sum = 0;
+            for (int i = start; i <= end; i++) {
+                sum = sum + i;
+                if (sum == N) {
+                    for (int j = start; j <= i; j++)
+                        System.out.print(j + " ");
+                    System.out.println();
+                    break;
+                }
+                if (sum > N)
+                    break;
+            }
+            sum = 0;
+            start++;
+        }
+    }
+
+    public static void main(String[] args) {
+        findConsecutive(100000);
+    }
+}
