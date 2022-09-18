4fd5ee0ec2dcb9b756e601f21b051faea0861c38
Krishna Lagad <krishnalagad2@gmail.com>
2022-09-18 13:53:27 +0530

    - Renaming directory

--- Java LeetCodePractice/SwapCorresponding.java
+++ Java LeetCodePractice/SwapCorresponding.java
@@ -1,6 +1,6 @@
 public class SwapCorresponding {
     static int[] intechange(int[] arr) {
-        for (int i = 0; i < arr.length - 1; i+=2) {
+        for (int i = 0; i < arr.length - 1; i += 2) {
             int temp = arr[i];
             arr[i] = arr[i + 1];
             arr[i + 1] = temp;
@@ -9,17 +9,17 @@ public class SwapCorresponding {
     }
 
     // public static int[] s(int[] arr) {
-    //     int[] answer = new int[100];
-    //     for (int i = 0; i < arr.length - 1; i += 2) {
-    //         int temp = arr[i];
-    //         answer[i] = arr[i + 1];
-    //         answer[i + 1] = temp;
-    //     }
-    //     return answer;
+    // int[] answer = new int[100];
+    // for (int i = 0; i < arr.length - 1; i += 2) {
+    // int temp = arr[i];
+    // answer[i] = arr[i + 1];
+    // answer[i + 1] = temp;
+    // }
+    // return answer;
     // }
 
     public static void main(String[] args) {
-        int[] arr = { 2, 3, 0, 1};
+        int[] arr = { 2, 3, 0, 1 };
         for (int i : arr)
             System.out.print(i + " ");
 
