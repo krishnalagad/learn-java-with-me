f60c5a04c7de074659fcdea4a3780fad38ece523
Krishna Lagad <krishnalagad2@gmail.com>
2022-09-18 13:38:22 +0530

    Update SwapCorresponding.java

--- Java LeetCodePractice/SwapCorresponding.java
+++ Java LeetCodePractice/SwapCorresponding.java
@@ -1,5 +1,5 @@
 public class SwapCorresponding {
-    static int[] changePos(int[] arr) {
+    static int[] intechange(int[] arr) {
         for (int i = 0; i < arr.length - 1; i+=2) {
             int temp = arr[i];
             arr[i] = arr[i + 1];
@@ -8,13 +8,23 @@ public class SwapCorresponding {
         return arr;
     }
 
+    // public static int[] s(int[] arr) {
+    //     int[] answer = new int[100];
+    //     for (int i = 0; i < arr.length - 1; i += 2) {
+    //         int temp = arr[i];
+    //         answer[i] = arr[i + 1];
+    //         answer[i + 1] = temp;
+    //     }
+    //     return answer;
+    // }
+
     public static void main(String[] args) {
-        int[] arr = { 2, 3, 0, 1, 7};
+        int[] arr = { 2, 3, 0, 1};
         for (int i : arr)
             System.out.print(i + " ");
 
         System.out.println();
-        arr = changePos(arr);
+        arr = intechange(arr);
         for (int i : arr)
             System.out.print(i + " ");
     }
