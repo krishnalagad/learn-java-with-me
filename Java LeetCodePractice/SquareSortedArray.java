16aaaf1e3e69e8951dd3bcb11a154535f34ff424
Krishna Lagad <krishnalagad2@gmail.com>
2022-09-20 22:37:46 +0530

    Create SquareSortedArray.java

---
+++ Java LeetCodePractice/SquareSortedArray.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.util.function.*;
+import java.util.stream.*;
+
+public class SquareSortedArray {
+    public static int[] sortedSquares(int[] nums) {
+        Function<Integer, Integer> square = x -> x * x;
+        List<Integer> list = Arrays.stream(nums).boxed().map(square).collect(Collectors.toList());
+        Collections.sort(list);
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { -4, -1, 0, 3, 10 };
+        arr = sortedSquares(arr);
+        for(int i: arr)
+            System.out.print(i + " ");
+    }
+}
