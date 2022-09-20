4fb827806f36443be3681da71b725a9f3aed496c
Krishna Lagad <krishnalagad2@gmail.com>
2022-09-20 23:10:54 +0530

    Update StringCapitalize.java

--- Java LeetCodePractice/StringCapitalize.java
+++ Java LeetCodePractice/StringCapitalize.java
@@ -1,15 +1,18 @@
 public class StringCapitalize {
     public static String capitalizeTitle(String title) {
-        String[] strArr = title.split("\\s");
         String capitalizeWord = "";
-        for(String str: strArr){
-            String initial = str.substring(0, 1);
-            String afterInitial = str.substring(1);
-            capitalizeWord += initial.toUpperCase() + afterInitial.toLowerCase() + " ";
+        for(String str: title.split("\\s")){
+            if (str.length() <=2) {
+                capitalizeWord += str.toLowerCase() + " ";
+            }else{
+                String initial = str.substring(0, 1);
+                String afterInitial = str.substring(1);
+                capitalizeWord += initial.toUpperCase() + afterInitial.toLowerCase() + " ";
+            }
         }
         return capitalizeWord.trim();
     }
     public static void main(String[] args) {
-        System.out.println(capitalizeTitle("capiTalIze tHe titLe"));
+        System.out.println(capitalizeTitle("First leTTeR of EACH Word"));
     }
 }
