39d2ee45d55d070149bc1d8ba2d726b738714c6e
Krishna Lagad <krishnalagad2@gmail.com>
2022-09-20 22:49:40 +0530

    Update SquareSortedArray.java

--- Java LeetCodePractice/SquareSortedArray.java
+++ Java LeetCodePractice/SquareSortedArray.java
@@ -3,15 +3,22 @@ import java.util.function.*;
 import java.util.stream.*;
 
 public class SquareSortedArray {
+    // public static int[] sortedSquares(int[] nums) {
+    //     Function<Integer, Integer> square = x -> x * x;
+    //     List<Integer> list = Arrays.stream(nums).boxed().map(square).collect(Collectors.toList());
+    //     Collections.sort(list);
+    //     return list.stream().mapToInt(Integer::intValue).toArray();
+    // }
+
     public static int[] sortedSquares(int[] nums) {
         Function<Integer, Integer> square = x -> x * x;
-        List<Integer> list = Arrays.stream(nums).boxed().map(square).collect(Collectors.toList());
-        Collections.sort(list);
+        // List<Integer> list = Arrays.stream(nums).boxed().map(square).collect(Collectors.toList());
+        Collections.sort(Arrays.stream(nums).boxed().map(square).collect(Collectors.toList()));
         return list.stream().mapToInt(Integer::intValue).toArray();
     }
 
     public static void main(String[] args) {
-        int[] arr = { -4, -1, 0, 3, 10 };
+        int[] arr = { -7, -3, 2, 3, 11 };
         arr = sortedSquares(arr);
         for(int i: arr)
             System.out.print(i + " ");
