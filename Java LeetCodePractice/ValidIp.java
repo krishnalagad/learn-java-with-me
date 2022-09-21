ae5c011c76053d0bfcaba266fa89ff94e8accdf4
Krishna Lagad <krishnalagad2@gmail.com>
2022-09-22 00:26:20 +0530

    Update ValidIp.java

--- Java LeetCodePractice/ValidIp.java
+++ Java LeetCodePractice/ValidIp.java
@@ -1,16 +1,17 @@
 public class ValidIp {
     public static String validIPAddress(String queryIP) {
         String answer = "";
-        if (queryIP.indexOf(".") == 1) {
+        if (queryIP.charAt(3) == '.') {
             String[] strArr = queryIP.split(".");
             for (String str : strArr) {
+                System.out.println(str);
                 if (Integer.parseInt(str) <= 255 && Integer.parseInt(str) >= 0) {
                     answer = "IPv4";
-                }else{
+                } else {
                     answer = "Neither";
                 }
             }
-        } else {
+        } else if (queryIP.indexOf(":") != -1) {
             answer = "IPv6";
             String[] strArr = queryIP.split(":");
         }
