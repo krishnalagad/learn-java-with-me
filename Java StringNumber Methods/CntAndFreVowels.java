705a7d9a5e3fcfce8bbaff6af98ca511665e792c
Krishna Lagad <krishnalagad2@gmail.com>
2022-09-23 22:15:53 +0530

    Update CntAndFreVowels.java

--- Java StringNumber Methods/CntAndFreVowels.java
+++ Java StringNumber Methods/CntAndFreVowels.java
@@ -1,8 +1,9 @@
 import java.util.*;
+import java.util.stream.*;
 public class CntAndFreVowels {
-    static Map<Character, Integer> resp = new HashMap<Character, Integer>(); // stores frequency of vowels
-
-    public static void checkFrequency(List<Character> list) {
+    static Map<String, Integer> resp = new HashMap<String, Integer>(); // stores frequency of vowels
+    
+    public static void checkFrequency(List<String> list) {
         for (int i = 0; i < list.size(); i++) {
             int count = 0;
             for (int j = 0; j < list.size(); j++)
@@ -13,28 +14,37 @@ public class CntAndFreVowels {
     }
 
     public static void solve(String name) {
-        char[] charArr = name.toCharArray();
-        List<Character> list1 = new ArrayList<>(); // stores vowels
-        List<Character> list2 = new ArrayList<>(); // stores consonents
-        for (char ch : charArr) {
-            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I'
-                    || ch == 'O' || ch == 'U')
-                list1.add(ch);
-            else if(ch == ' ')
-                continue;
-            else
-                list2.add(ch);
-        }
-        checkFrequency(list1);
-        System.out.println("Vowels: " + list1);
-        System.out.println("Consonent: " + list2);
+        String vowels = name.replaceAll("(?i)[^aeiou]", "");
+        String consonents = name.replaceAll("(?i)[aeiou]", "");
+        // converting vowels string into string array using split() and then converting it to List<String>
+        checkFrequency(Arrays.stream(vowels.split("")).collect(Collectors.toList()));   
+        System.out.println("Vowels: " + vowels + "\nConsonents: " + consonents);
     }
 
     public static void main(String[] args) {
-        solve("aakanksha aishwarya");
+        solve("aakankshaaishwarya");
         System.out.println("Vowel\t\tFrequency");
         resp.forEach((k, v) -> {
             System.out.println(k + "\t->\t" + v);
         });
     }
 }
+
+// public static void solve(String name) {
+// char[] charArr = name.toCharArray();
+// List<Character> list1 = new ArrayList<>(); // stores vowels
+// List<Character> list2 = new ArrayList<>(); // stores consonents
+// for (char ch : charArr) {
+// if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch ==
+// 'A' || ch == 'E' || ch == 'I'
+// || ch == 'O' || ch == 'U')
+// list1.add(ch);
+// else if(ch == ' ')
+// continue;
+// else
+// list2.add(ch);
+// }
+// checkFrequency(list1);
+// System.out.println("Vowels: " + list1);
+// System.out.println("Consonent: " + list2);
+// }
