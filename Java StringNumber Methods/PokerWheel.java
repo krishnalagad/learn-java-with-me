85ebcbf895e5beda69326e842b4c76feec70d1b4
Krishna Lagad <krishnalagad2@gmail.com>
2022-09-23 21:11:20 +0530

    Create PokerWheel.java

---
+++ Java StringNumber Methods/PokerWheel.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class PokerWheel {
+    public static int[] solve(String[] strArr) {
+        List<List<Integer>> list1 = new ArrayList<>();
+        List<Integer> answer = new ArrayList<>();
+        for (String str : strArr) {
+            String[] charArr = str.split("");
+            List<Integer> list2 = new ArrayList<>();
+            for (String ch : charArr) {
+                list2.add(Integer.parseInt(ch));
+            }
+            Collections.sort(list2, Collections.reverseOrder());
+            list1.add(list2);
+        }
+        for (List<Integer> temp : list1) {
+            answer.add(temp.get(0));
+        }
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public static void main(String[] args) {
+        String[] strArr = {"123", "234", "432"};
+        int[] arr = new int[strArr.length];
+        arr = solve(strArr);
+        for(int i: arr){
+            System.out.print(i + " ");
+        }
+    }
+}
