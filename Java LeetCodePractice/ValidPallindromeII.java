76504f694853b90fe1a0a48b197c90f4034b789c
Krishna Lagad <krishnalagad2@gmail.com>
2022-09-27 23:21:48 +0530

    Update ValidPallindromeII.java

--- Java LeetCodePractice/ValidPallindromeII.java
+++ Java LeetCodePractice/ValidPallindromeII.java
@@ -26,3 +26,32 @@ public class ValidPallindromeII {
                 "tkhyzcuolcevmcxqngiemrezsmlrqrwutwbecinfrolmqmnlzglihredxpgidwipbrkqygkcaiuwnshgqetrvgdqhkxgzugjmjlqxdaonegeugqrvowldihcvjtalbmrnypgwimkcmnwmtkuolpcgeievxewnpgwnuppfzzildfazkjebowdjjtxuqcndzrwbwymnveyedftuibheazgbqdfwtozfjwvnhblrlclfqndaiebuymyezahxsdmrxifkwtcxgqmdxfcfuojobekknkoambznbghijslaeyqpbkhdoovgxwihlojkebimntvjpnluacxlnzwhfcqtnuvzvjpkazwotvbvbtwnzrssnqtyniaokpkqqpwpzfnojayozjoomwnknhkphnyvkadsdrkngxjwietzmuvkoozzyoydbiwyqgxlutfugsaxikufubkyucnbkpbjffzivqpuzfwilhmncpcrytunpryzxohfnccbllyhskpszygjgfrfczhjgszasewngubrsxojiqphbqciaooenbstkdblffishfpyxwwluihkxoilmfkrpgpybtyhkvyyjtonakvmvwveectvkrljklvkqthmevkkdjtzfryqghkpjsuzlobdscrormuclxpqgdcrzkjihyroatfmqzqtqebzulhrdiibjuxzbkjkkgfymqqyxpcoezucdbrfkadkyxovjzhqbfcnmhcmgjokigpsgzwrosrfbztxznqdgreldcrmciipvnafnkrpxzdjolkmuynskmemdhlsliklffrifxrsvchuozttsuantjixiilvzsgzqtegpwriqybvmmlktgkdriwpyjdxjngvejfrnmhhglcltdlorbuaaqhilylywjujgwwlagfxmboixetgsgtwdtxcehcpywjmzqfsnqhpbgclossccnnrkdetpjovkpkwjuwxknouyytdliosfyjqhmyndloscdsydhvddfoofddvhdysdcsoldnymhqjyfsoildtyyuonkxwujwkpkvojptedkrnnccssolcgbphqnsfqzmjwypchecxtdwtgsgtexiobmxfgalwwgjujwylylihqaaubroldtlclghhmnrfjevgnjxdjypwirdkgtklmmvbyqirwpgetqzgszvliixijtnausttzouhcvsrxfirfflkilslhdmemksnyumklojdzxprknfanvpiicmrcdlergdqnzxtzbfrsorwzgspgikojgmchmncfbqhzjvoxykdakfrbdcuzeocpxyqqmyfgkkjkbzxujbiidrhluzbeqtqzqmftaoryhijkzrcdgqpxlcumrorcsdbolzusjpkhgqyrfztjdkkvemhtqkvlkjlrkvtceevwvmvkanotjyyvkhytbypgprkfmlioxkhiulwwxypfhsifflbdktsbneooaicqbhpqijoxsrbugnwesazsgjhzcfrfgjgyzspkshyllbccnfhoxzyrpnutyrcpcnmhliwfzupqvizffjbpkbncuykbufukixasguftulxgqywibdyoyzzookvumzteiwjxgnkrdsdakvynhpkhnknwmoojzoyajonfzpwpqqkpkoainytqnssrznwtbvbvtowzakpjvzvuntqcfhwznlxcaulnpjvtnmibekjolhiwxgvoodhkbpqyealsjihgbnzbmaoknkkebojoufcfxdmqgxctwkfixrmdsxhazeymyubeiadnqflclrlbhnvwjfzotwfdqbgzaehbiutfdeyevnmywbwrzdncquxtjjdwobejkzafdlizzfppunwgpnwexveiegcplouktmwnmckmiwgpynrmblatjvchidlwovrqguegenoadxqljmjguzgxkhqdgvrteqghsnwuiackgyqkrbpiwdigpxderhilgzlnmqmlorfnicebwtuwrqrlmszermeignqxcmveclouczyhkt"));
     }
 }
+
+/*
+ * public boolean validPalindrome(String s) {
+ * int left = 0, right = s.length() - 1;
+ * 
+ * while (left < right) {
+ * if (s.charAt(left) != s.charAt(right))
+ * return subPalindrome(s, left + 1, right) || subPalindrome(s, left, right -
+ * 1);
+ * 
+ * left++;
+ * right--;
+ * }
+ * 
+ * return true;
+ * }
+ * 
+ * public boolean subPalindrome(String s, int left, int right) {
+ * while (left < right) {
+ * if (s.charAt(left) != s.charAt(right))
+ * return false;
+ * 
+ * left++;
+ * right--;
+ * }
+ * 
+ * return true;
+ * }
+ */
