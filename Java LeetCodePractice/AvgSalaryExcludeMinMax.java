9f0e6db8fdca7b0e8305e6b28f8a6de7d726ad02
Krishna Lagad <krishnalagad2@gmail.com>
2022-09-27 23:39:37 +0530

    Update AvgSalaryExcludeMinMax.java

--- Java LeetCodePractice/AvgSalaryExcludeMinMax.java
+++ Java LeetCodePractice/AvgSalaryExcludeMinMax.java
@@ -6,17 +6,17 @@ public class AvgSalaryExcludeMinMax {
         List<Integer> res = new ArrayList<Integer>();
         List<Integer> list = Arrays.stream(salary).boxed().collect(Collectors.toList());
         Collections.sort(list);
-        for (int i = 0; i < salary.length; i++) {
+        for (int i = 0; i < list.size(); i++) {
             if (i == 0 || i == salary.length - 1)
                 continue;
             else
                 res.add(list.get(i));
         }
-        return res.stream().mapToInt(Integer::intValue).sum() / 2;
+        return res.stream().mapToInt(Integer::intValue).sum() / res.size();
     }
 
     public static void main(String[] args) {
-        int[] arr = { 4000, 3000, 1000, 2000 };
+        int[] arr = { 1000, 2000, 3000 };
         System.out.println(average(arr));
     }
 }
