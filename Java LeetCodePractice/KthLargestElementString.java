7e1523c44d2a98117eaab717ec6bbe68f3b98f8b
Krishna Lagad <krishnalagad2@gmail.com>
2022-09-28 22:04:11 +0530

    Update KthLargestElementString.java

--- Java LeetCodePractice/KthLargestElementString.java
+++ Java LeetCodePractice/KthLargestElementString.java
@@ -7,6 +7,14 @@ public class KthLargestElementString {
         return String.valueOf(arr[arr.length - k]);
     }
 
+    // def kthLargestNumber(self, nums: List[str], k: int) -> str:
+    //     heap, res = [], math.inf
+    //     for num in nums:
+    //         heappush(heap, -int(num))
+    //     for i in range(min(len(heap), k)):
+    //         res = -heappop(heap)
+    //     return str(res)
+
     public static void main(String[] args) {
         String[] arr = { "623986800","3","887298","695","794","6888794705","269409","59930972","723091307","726368","8028385786","378585"};
         System.out.println(kthLargestNumber(arr, 3));
