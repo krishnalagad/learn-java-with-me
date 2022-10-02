cc95fdcd1394d1876c4e8e4aaf24cc2870f6dee4
Krishna Lagad <krishnalagad2@gmail.com>
2022-10-02 16:22:31 +0530

    Update Triangle.java

--- Java LeetCodePractice/Triangle.java
+++ Java LeetCodePractice/Triangle.java
@@ -2,20 +2,30 @@ import java.util.*;
 
 public class Triangle {
     public static int minimumTotal(List<List<Integer>> triangle) {
-        int total = 0;
-        for (List<Integer> tr : triangle) {
-            Collections.sort(tr);
-            total += tr.get(0);
+        // int total = 0;
+        // for (List<Integer> tr : triangle) {
+        //     Collections.sort(tr);
+        //     System.out.println(tr.get(0));
+        //     total += tr.get(0);
+        // }
+        // return total;
+
+        for (int i = triangle.size() - 2; i >= 0; i--) {
+            for (int j = 0; j < triangle.get(i).size(); j++) {
+                triangle.get(i).set(j, triangle.get(i).get(j) + Math.min(
+                        triangle.get(i + 1).get(j),
+                        triangle.get(i + 1).get(j + 1)));
+            }
         }
-        return total;
+        return triangle.get(0).get(0);
     }
 
     public static void main(String[] args) {
         List<List<Integer>> triangle = new ArrayList<>();
-        triangle.add(new ArrayList<>(Arrays.asList(2)));
-        triangle.add(new ArrayList<>(Arrays.asList(3, 4)));
-        triangle.add(new ArrayList<>(Arrays.asList(6, 5, 7)));
-        triangle.add(new ArrayList<>(Arrays.asList(4, 1, 8, 3)));
+        triangle.add(new ArrayList<>(Arrays.asList(-1)));
+        triangle.add(new ArrayList<>(Arrays.asList(2, 3)));
+        triangle.add(new ArrayList<>(Arrays.asList(1, -1, -3)));
+        // triangle.add(new ArrayList<>(Arrays.asList(4, 1, 8, 3)));
         System.out.println(minimumTotal(triangle));
     }
 }
