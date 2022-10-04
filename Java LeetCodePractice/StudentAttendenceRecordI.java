b8186898e45f3473fada85c0dde08636176ba97c
Krishna Lagad <krishnalagad2@gmail.com>
2022-10-04 21:16:51 +0530

    Create StudentAttendenceRecordI.java

---
+++ Java LeetCodePractice/StudentAttendenceRecordI.java
@@ -0,0 +1,25 @@
+public class StudentAttendenceRecordI {
+    public static boolean checkRecord(String s) {
+        int absent = 0, late = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == 'L') {
+                late += 1;
+            } else {
+                late = 0;
+                if (ch == 'A') {
+                    absent += 1;
+                }
+            }
+            if (absent >= 3 || late >= 3) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(checkRecord("PPALLL"));
+    }
+}
