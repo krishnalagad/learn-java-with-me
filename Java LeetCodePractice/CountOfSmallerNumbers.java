1a2dfd17ff35f9e97809103572502b1ed53156f4
Krishna Lagad <krishnalagad2@gmail.com>
2022-10-08 22:34:53 +0530

    Create CountOfSmallerNumbers.java

---
+++ Java LeetCodePractice/CountOfSmallerNumbers.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class CountOfSmallerNumbers {
+    public List<Integer> countSmaller(int[] nums) {
+        return new ArrayList();
+    }
+    public static void main(String[] args) {
+        
+    }
+}
