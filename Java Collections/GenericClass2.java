d5814d7f6901b4e01aee692b2e3e4c985fd886f8
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-12 10:34:10 +0530

    Update GenericClass2.java
    - Float Type

--- Java Collections/GenericClass2.java
+++ Java Collections/GenericClass2.java
@@ -1,3 +1,28 @@
 public class GenericClass2 {
-    
+    static class Type2<T> {
+        private T var1;
+
+        public Type2() {
+
+        }
+
+        public Type2(T var1) {
+            this.var1 = var1;
+        }
+
+        public void setVar(T var1) {
+            this.var1 = var1;
+        }
+
+        public T getVar() {
+            return this.var1;
+        }
+    }
+
+    public static void main(String[] args) {
+        Type2<Float> type2 = new Type2<>(12.8f);
+        System.out.println(type2.getVar());
+
+        
+    }
 }
