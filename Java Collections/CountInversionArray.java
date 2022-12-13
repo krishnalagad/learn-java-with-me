3e15088d1e0251bbf696b0798a89c6644dd3d643
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-13 22:56:45 +0530

    Update CountInversionArray.java

--- Java Collections/CountInversionArray.java
+++ Java Collections/CountInversionArray.java
@@ -1,21 +1,20 @@
 import java.util.*;
 
 public class CountInversionArray {
-    public static int solve(int[] nums){
+    public static int solve(int[] nums) {
         List<List<Integer>> resp = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            for(int j = i; j < nums.length; j++) {
+        for (int i = 0; i < nums.length; i++)
+            for (int j = i; j < nums.length; j++)
                 if (nums[i] > nums[j]) {
                     List<Integer> list = Arrays.asList(nums[i], nums[j]);
                     resp.add(list);
                 }
-            }
-        }
 
         return resp.size();
     }
+
     public static void main(String[] args) {
-        int[] nums = {8, 4, 2, 1};
+        int[] nums = { 8, 4, 2, 1 };
         System.out.println(solve(nums));
     }
 }
