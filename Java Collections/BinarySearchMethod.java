30c86d5f5681b348e3e0521adfcd3510c58ca4e0
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-13 22:32:03 +0530

    Update BinarySearchMethod.java

--- Java Collections/BinarySearchMethod.java
+++ Java Collections/BinarySearchMethod.java
@@ -7,7 +7,7 @@ public class BinarySearchMethod {
      * @param args
      */
     public static void main(String[] args) {
-        int[] arr = new int[10000];
+        int[] arr = new int[100000];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = i + 1;
         }
