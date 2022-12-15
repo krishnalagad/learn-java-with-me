f0f2e954f113abf5235af450b4a7764ab1120bfc
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-15 22:30:43 +0530

    Update CapitalizeWord.java

--- Java LeetCodePractice/CapitalizeWord.java
+++ Java LeetCodePractice/CapitalizeWord.java
@@ -3,22 +3,21 @@ public class CapitalizeWord {
         String[] strArr = str.split(" ");
         String result = "";
         for (String s : strArr) {
-            if (s.length() == 1) {
+            if (s.length() == 1)
                 result += Character.toLowerCase(s.charAt(0)) + " ";
-            } else if (s.length() == 2) {
+            else if (s.length() == 2)
                 result += s.toLowerCase() + " ";
-            } else {
+            else {
                 String cap = s.substring(0, 1).toUpperCase();
                 String small = s.substring(1).toLowerCase();
                 result += cap + small + " ";
             }
-
         }
-        return result;
+        return result.trim();
     }
 
     public static void main(String[] args) {
-        String result = makeCapitalize("I loVe yoU");
+        String result = makeCapitalize("My name iS kriSHNa");
         System.out.println(result);
     }
 
