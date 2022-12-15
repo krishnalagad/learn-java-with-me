ee79c9a82414b077cf221dc59177f4cd02f1bd28
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-15 19:17:28 +0530

    Update Practice1.java

--- Java LeetCodePractice/Practice1.java
+++ Java LeetCodePractice/Practice1.java
@@ -1,4 +1,5 @@
 import java.io.*;
+
 public class Practice1 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -6,26 +7,33 @@ public class Practice1 {
         String[] strArr = str.split(" ");
         int a = 0;
         for (String s : strArr) {
-            int count = 0;
+            int count1 = 0;
+            int count2 = 0;
             for (int i = 0; i < s.length(); i++) {
                 if (s.length() < 8)
-                    count++;
+                    count1++;
                 else {
+
                     if (Character.isLetter(s.charAt(i))) {
-                        count++;
+                        count1++;
                     } else {
-                        count = 0;
+                        count1 = 0;
+                    }
+                    if (Character.isDigit(s.charAt(i))) {
+                        count2++;
+                    } else {
+                        count2 = 0;
                     }
                 }
-                if (count > 2) {
+                if (count1 >= 3 || count2 >= 3)
                     a++;
-                }
             }
+            System.out.println("a: " + a);
         }
-        if (a > (strArr.length / 2)) {
-            System.out.println("Strong");
-        } else {
+        if (a >= (strArr.length / 2)) {
             System.out.println("Weak");
+        } else {
+            System.out.println("Strong");
         }
 
         System.out.println(str);
