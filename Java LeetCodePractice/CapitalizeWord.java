13f6afe3edc49fdf623be038e41c8756e1d12900
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-15 22:18:58 +0530

    Create CapitalizeWord.java

---
+++ Java LeetCodePractice/CapitalizeWord.java
@@ -0,0 +1,25 @@
+public class CapitalizeWord {
+    private static String makeCapitalize(String str) {
+        String[] strArr = str.split(" ");
+        String result = "";
+        for (String s : strArr) {
+            if (s.length() == 1) {
+                result += Character.toLowerCase(s.charAt(0)) + " ";
+            } else if (s.length() == 2) {
+                result += s.toLowerCase() + " ";
+            } else {
+                String cap = s.substring(0, 1).toUpperCase();
+                String small = s.substring(1).toLowerCase();
+                result += cap + small + " ";
+            }
+
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String result = makeCapitalize("I loVe yoU");
+        System.out.println(result);
+    }
+
+}
