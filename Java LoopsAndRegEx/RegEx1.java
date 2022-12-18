634e62e4d7ea0df695c9508bb28119ed5ba521be
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-18 11:28:58 +0530

    Update RegEx1.java

--- Java LoopsAndRegEx/RegEx1.java
+++ Java LoopsAndRegEx/RegEx1.java
@@ -1,8 +1,10 @@
 import java.util.regex.*;
 
 public class RegEx1 {
-    public static int findMatches(String[] strArr){
+    public static int findMatches(String[] strArr) {
         Pattern pattern = Pattern.compile("([a-z\\d])\\1\\1", Pattern.CASE_INSENSITIVE);
+        // Pattern pattern = Pattern.compile("\\b([a-zA-Z0-9])\\1\\1+\\b",
+        // Pattern.CASE_INSENSITIVE);
         int count = 0;
         for (String str : strArr) {
             Matcher matcher = pattern.matcher(str);
@@ -15,6 +17,7 @@ public class RegEx1 {
         }
         return count;
     }
+
     public static void main(String[] args) {
         String[] strArr = { "aaa", "111", "abc", "222", "ccc" };
         System.out.println(findMatches(strArr));
