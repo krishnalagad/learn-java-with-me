9764e89535529d8f9c9e88e1576715332e99ba2d
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-18 11:24:02 +0530

    Update RegEx1.java

--- Java LoopsAndRegEx/RegEx1.java
+++ Java LoopsAndRegEx/RegEx1.java
@@ -1,4 +1,22 @@
 import java.util.regex.*;
-public class RegEx1{
-    
+
+public class RegEx1 {
+    public static int findMatches(String[] strArr){
+        Pattern pattern = Pattern.compile("([a-z\\d])\\1\\1", Pattern.CASE_INSENSITIVE);
+        int count = 0;
+        for (String str : strArr) {
+            Matcher matcher = pattern.matcher(str);
+            boolean matchFound = matcher.find();
+            if (matchFound) {
+                count += 1;
+            } else {
+                continue;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        String[] strArr = { "aaa", "111", "abc", "222", "ccc" };
+        System.out.println(findMatches(strArr));
+    }
 }
