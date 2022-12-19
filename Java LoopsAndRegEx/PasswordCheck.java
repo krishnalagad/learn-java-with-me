70b9b3e5116ebf857e60e140fa65b5b02c21ca6c
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-19 20:47:00 +0530

    Create PasswordCheck.java

---
+++ Java LoopsAndRegEx/PasswordCheck.java
@@ -0,0 +1,8 @@
+import java.io.*;
+import java.util.*;
+public class PasswordCheck{
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        
+    }
+}
