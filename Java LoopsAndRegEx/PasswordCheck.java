8d18716aedc12c0d192a4c70778aee40f9657863
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-19 21:04:19 +0530

    Update PasswordCheck.java

--- Java LoopsAndRegEx/PasswordCheck.java
+++ Java LoopsAndRegEx/PasswordCheck.java
@@ -1,8 +1,34 @@
 import java.io.*;
-import java.util.*;
-public class PasswordCheck{
-    public static void main(String[] args) {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        
+
+public class PasswordCheck {
+    public static void checkPassword(String password) {
+        String[] strArr = password.split(" ");
+        int a = 0;
+        for (String str : strArr) {
+            int count = 0;
+            if (str.length() < 8) {
+                count = 1;
+            }
+            if (count == 1) {
+                a += 1;
+            }
+            for (int i = 2; i < str.length(); i++) {
+
+                if (Character.isLetter(str.charAt(i)) && Character.isLetter(str.charAt(i - 1))
+                        && Character.isLetter(str.charAt(i - 2))) {
+                    count = 1;
+                }else{
+                    count = 0;
+                }
+                
+            }
+            System.out.println("a: " + a);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        // BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        // String str = reader.readLine();
+        checkPassword("pass1 ancs1234 1q2w3e4r5t");
     }
 }
