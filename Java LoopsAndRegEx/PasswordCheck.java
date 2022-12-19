4947f129f73ad12e8d64db30e5f9308885505fc6
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-19 21:56:58 +0530

    - Working

--- Java LoopsAndRegEx/PasswordCheck.java
+++ Java LoopsAndRegEx/PasswordCheck.java
@@ -6,16 +6,16 @@ public class PasswordCheck {
         int a = 0;
         for (String str : strArr) {
             int count = 0;
-            if (str.length() < 8) 
+            if (str.trim().length() < 8)
                 a += 1;
             else {
-                for (int i = 2; i < str.length(); i++) {
-                    if ((Character.isLetter(str.charAt(i)) && Character.isLetter(str.charAt(i - 1)) 
+                for (int i = 2; i < str.trim().length(); i++) {
+                    if ((Character.isLetter(str.charAt(i)) && Character.isLetter(str.charAt(i - 1))
                             && Character.isLetter(str.charAt(i - 2)))
-                        || (Character.isDigit(str.charAt(i)) && Character.isDigit(str.charAt(i - 1))
-                            && Character.isDigit(str.charAt(i - 2)))) 
+                            || (Character.isDigit(str.charAt(i)) && Character.isDigit(str.charAt(i - 1))
+                                    && Character.isDigit(str.charAt(i - 2))))
                         count = 1;
-                    
+
                     if (count == 1) {
                         a += 1;
                         break;
@@ -23,15 +23,15 @@ public class PasswordCheck {
                 }
             }
         }
-        if (a > Double.valueOf(strArr.length / Double.valueOf(2))) 
+        if (a > Double.valueOf(strArr.length / Double.valueOf(2)))
             System.out.println("Weak");
-        else 
+        else
             System.out.println("Strong");
     }
 
     public static void main(String[] args) throws IOException {
         // BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         // String str = reader.readLine();
-        checkPassword("a 1234ab67cd a23s4d5f6 1q2w3e4r5t abcd1234 e3r4t5y6");   // string of input passwords
+        checkPassword("a 1234ab67cd a23s4d5f6 1q2w3e4r5t e23r4t5y6"); // string of input passwords
     }
 }
