3b06a9d59fc17cffcb3ce33363e86632b7de9194
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-21 20:18:37 +0530

    - Pattern 4

--- Java LoopsRegExPatterns/Pattern4.java
+++ Java LoopsRegExPatterns/Pattern4.java
@@ -1,5 +1,15 @@
 public class Pattern4 {
-    public static void main(String[] args) {
-        
+    private static void pattern4(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n - row + 1; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
+
+    public static void main(String[] args) {
+        pattern4(5);
+    }
+
 }
