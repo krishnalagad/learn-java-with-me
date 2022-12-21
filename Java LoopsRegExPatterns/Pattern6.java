d17bb179b654a17dfeaa1aae5445fae44fc6490c
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-21 20:34:17 +0530

    Update Pattern6.java

--- Java LoopsRegExPatterns/Pattern6.java
+++ Java LoopsRegExPatterns/Pattern6.java
@@ -4,6 +4,12 @@ public class Pattern6 {
     }
 
     private static void pattern6(int n) {
-        
+        for (int row = 1; row <= 2 * n - 1; row++) {
+            int colAtEachRow = row > n ? 2 * n - row : row;     // if loops starts from 0 then (2*n-row-1)
+            for (int col = 1; col <= colAtEachRow; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
