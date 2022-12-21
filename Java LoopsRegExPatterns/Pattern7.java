d19635c0873ef1070d5ef80afd3854537fa72ea5
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-21 20:43:42 +0530

    - Pattern 7

--- Java LoopsRegExPatterns/Pattern7.java
+++ Java LoopsRegExPatterns/Pattern7.java
@@ -4,6 +4,16 @@ public class Pattern7 {
     }
 
     private static void pattern7(int n) {
-        
+        for (int row = 1; row < 2 * n + 1; row++) {
+            int colAtEachRow = row > n ? 2 * n - row : row; // if loops starts from 0 then (2*n-row-1)
+            int noOfSpaces = n - colAtEachRow;
+            for (int i = 1; i <= noOfSpaces; i++) {
+                System.out.print(" ");
+            }
+            for (int col = 1; col <= colAtEachRow; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
