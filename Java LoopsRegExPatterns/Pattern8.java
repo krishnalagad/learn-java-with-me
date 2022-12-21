e993ac47334d1cbaff677990de7471f766bcf5c6
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-21 21:03:22 +0530

    - Pattern 8

--- Java LoopsRegExPatterns/Pattern8.java
+++ Java LoopsRegExPatterns/Pattern8.java
@@ -3,5 +3,18 @@ public class Pattern8 {
         pattern8(5);
     }
 
-    
+    private static void pattern8(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int spaces = 1; spaces <= n - row; spaces++) {
+                System.out.print("  ");
+            }
+            for (int col = row; col >= 1; col--) {
+                System.out.print(col + " ");
+            }
+            for (int col = 2; col <= row; col++) {
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
+    }
 }
