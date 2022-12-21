b891a3f085e7b10a6bca28986c1a4a098252a1c2
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-21 19:28:26 +0530

    - Pattern 1
    1 2 3 4 5
     2 3 4 5
      3 4 5
       4 5
        5
       4 5
      3 4 5
     2 3 4 5
    1 2 3 4 5

--- Java LoopsAndRegEx/Patterns.java
+++ Java LoopsAndRegEx/Patterns.java
@@ -1,3 +1,28 @@
 public class Patterns {
-    
+    private static void pattern1(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = i; j <= n; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j < i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = i; j <= n; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        pattern1(5);
+    }
 }
