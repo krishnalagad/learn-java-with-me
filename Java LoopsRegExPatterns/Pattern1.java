8156dd740511cb7b10532db30ef82029496e302a
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-21 19:54:24 +0530

    Update Pattern1.java

--- Java LoopsRegExPatterns/Pattern1.java
+++ Java LoopsRegExPatterns/Pattern1.java
@@ -3,27 +3,21 @@ import java.util.Scanner;
 public class Pattern1 {
     private static void printPattern(int n) {
         for (int i = 1; i <= n; i++) {
-            // i-1 spaces in each row
             for (int j = 1; j < i; j++) {
                 System.out.print(" ");
             }
-            // System.out.print(i);
-            // Loop to iterate over each column
+
             for (int j = i; j <= n; j++) {
                 System.out.print(j + " ");
             }
             System.out.println();
         }
 
-        // Printing Lower Half of n-1 rows
-        // Loop to iterate over each row in reverse order
         for (int i = n - 1; i >= 1; i--) {
-            // i-1 spaces in each row
             for (int j = 1; j < i; j++) {
                 System.out.print(" ");
             }
 
-            // Loop to iterate over each column
             for (int j = i; j <= n; j++) {
                 System.out.print(j + " ");
             }
