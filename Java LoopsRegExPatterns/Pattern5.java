644959b14e0d125be1f5e8f2484ffbd2979f3d97
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-21 20:23:07 +0530

    - Pattern 5

--- Java LoopsRegExPatterns/Pattern5.java
+++ Java LoopsRegExPatterns/Pattern5.java
@@ -1,5 +1,13 @@
 public class Pattern5 {
+    public static void pattern5(int n){
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
-        
+        pattern5(5);
     }
 }
