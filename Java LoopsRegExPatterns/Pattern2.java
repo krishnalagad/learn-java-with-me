d758f2cc0e440b6ae8a8293dd1289b9e713b113f
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-21 19:49:51 +0530

    - Pattern 2

---
+++ Java LoopsRegExPatterns/Pattern2.java
@@ -0,0 +1,14 @@
+public class Pattern2 {
+    public static void pattern2(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        pattern2(5);
+    }
+}
