3b88786bcf8a54494874896f8b4e2315f6b8a603
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-21 21:10:58 +0530

    _ Pattern 9

--- Java LoopsRegExPatterns/Pattern9.java
+++ Java LoopsRegExPatterns/Pattern9.java
@@ -3,6 +3,20 @@ public class Pattern9 {
         pattern9(5);
     }
 
-    private static void pattern9(int i) {
+    private static void pattern9(int n) {
+        for (int row = 1; row <= n * 2; row++) {
+            int c = row > n ? 2 * n - row : row;
+
+            for (int spaces = 1; spaces <= n - c; spaces++) {
+                System.out.print("  ");
+            }
+            for (int col = c; col >= 1; col--) {
+                System.out.print(col + " ");
+            }
+            for (int col = 2; col <= c; col++) {
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
     }
 }
