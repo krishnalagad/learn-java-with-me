ae42166aea459c0b609b5bbe0b2b9ee51efae7fe
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-21 20:11:10 +0530

    Update Pattern3.java

--- Java LoopsRegExPatterns/Pattern3.java
+++ Java LoopsRegExPatterns/Pattern3.java
@@ -1,6 +1,11 @@
 public class Pattern3 {
     public static void pattern3(int n){
-        
+        for(int row = 0; row < n; row++){
+            for (int col = 1; col <= n; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
     public static void main(String[] args) {
         pattern3(5);
