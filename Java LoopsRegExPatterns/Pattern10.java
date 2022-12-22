3299bbc7e84f883a3508b394db2fd04772ad8367
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-22 19:32:19 +0530

    - Pattern 10

--- Java LoopsRegExPatterns/Pattern10.java
+++ Java LoopsRegExPatterns/Pattern10.java
@@ -1,8 +1,16 @@
 public class Pattern10 {
     public static void main(String[] args) {
-        pattern10(5);
+        pattern10(4);
     }
 
     private static void pattern10(int n) {
+        n = 2 * n;
+        for (int row = 0; row <= n; row++) {
+            for (int col = 0; col <= n; col++) {
+                int atEachColValue = Math.min(Math.min(row, col), Math.min(n - row, n - col));
+                System.out.print(atEachColValue + " ");
+            }
+            System.out.println();
+        }
     }
 }
