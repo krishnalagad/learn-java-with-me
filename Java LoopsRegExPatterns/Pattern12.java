855a3a392af4eb3c4b438d0aa8b23dc650c7c97c
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-23 18:44:59 +0530

    - Pattern 12
    - Output:
    * * * *
    *      *
    *      *
    * * * *

--- Java LoopsRegExPatterns/Pattern12.java
+++ Java LoopsRegExPatterns/Pattern12.java
@@ -1,5 +1,17 @@
-public class Pattern12{
+public class Pattern12 {
     public static void main(String[] args) {
-        
+        pattern12(4);
+    }
+
+    private static void pattern12(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n ; col++) {
+                if (row == 1 || col == 1 || row == n || col == n) 
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+            }
+            System.out.println();
+        }
     }
 }
