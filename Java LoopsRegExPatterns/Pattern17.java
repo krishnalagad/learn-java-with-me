8c6dd5ed823507aba0a77949732309dfd65a3129
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-23 19:45:04 +0530

    - Pattern 17

--- Java LoopsRegExPatterns/Pattern17.java
+++ Java LoopsRegExPatterns/Pattern17.java
@@ -1,5 +1,34 @@
 public class Pattern17 {
     public static void main(String[] args) {
-        
+        pattern17(5);
+    }
+
+    private static void pattern17(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print("* ");     // 1 space after *.
+            }
+            int spaces = 2 * (n - row);
+            for (int col = 1; col <= spaces; col++) {
+                System.out.print(" ");
+            }
+            for(int col = 1; col <= row; col++) {
+                System.out.print("* ");     // 1 space after *.
+            }
+            System.out.println();
+        }
+        for (int row = n - 1; row >= 1; row--) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print("* ");     // 1 space after *.
+            }
+            int spaces = 2 * (n - row);
+            for (int col = 1; col <= spaces; col++) {
+                System.out.print(" ");
+            }
+            for (int col = 1; col <= row; col++) {
+                System.out.print("* ");     // 1 space after *.
+            }
+            System.out.println();
+        }
     }
 }
