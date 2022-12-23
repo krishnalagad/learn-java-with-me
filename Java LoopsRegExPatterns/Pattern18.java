58fc4b4e9d301554186e84fb1b91d68e8b2289e2
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-23 19:48:24 +0530

    - Pattern 18

--- Java LoopsRegExPatterns/Pattern18.java
+++ Java LoopsRegExPatterns/Pattern18.java
@@ -1,5 +1,35 @@
 public class Pattern18 {
     public static void main(String[] args) {
-        
+        pattern18(5);
     }
+
+    private static void pattern18(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print("*"); // no space after *.
+            }
+            int spaces = 2 * (n - row);
+            for (int col = 1; col <= spaces; col++) {
+                System.out.print(" ");
+            }
+            for (int col = 1; col <= row; col++) {
+                System.out.print("*"); // no space after *.
+            }
+            System.out.println();
+        }
+        for (int row = n - 1; row >= 1; row--) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print("*"); // no space after *.
+            }
+            int spaces = 2 * (n - row);
+            for (int col = 1; col <= spaces; col++) {
+                System.out.print(" ");
+            }
+            for (int col = 1; col <= row; col++) {
+                System.out.print("*"); // no space after *.
+            }
+            System.out.println();
+        }
+    }
+
 }
