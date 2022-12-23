664617da08f2d464137cc4ba2733322453394bba
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-23 18:55:09 +0530

    - Pattern 13

--- Java LoopsRegExPatterns/Pattern13.java
+++ Java LoopsRegExPatterns/Pattern13.java
@@ -1,3 +1,17 @@
 public class Pattern13 {
-    
+    public static void main(String[] args) {
+        pattern13(7);
+    }
+
+    private static void pattern13(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int spaces = 1; spaces <= n - row; spaces++) {
+                System.out.print(" ");
+            }
+            for (int col = 1; col <= row; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
 }
