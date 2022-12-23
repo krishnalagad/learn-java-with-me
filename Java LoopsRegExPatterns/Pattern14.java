f47431bbafe70c1fffb9bbbd54c6a27bc81be834
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-23 18:56:45 +0530

    - Pattern 14

--- Java LoopsRegExPatterns/Pattern14.java
+++ Java LoopsRegExPatterns/Pattern14.java
@@ -1,3 +1,17 @@
 public class Pattern14 {
-    
+    public static void main(String[] args) {
+        pattern14(7);
+    }
+
+    private static void pattern14(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int spaces = 1; spaces <= n - row; spaces++) {
+                System.out.print("  "); // two spaces here.
+            }
+            for (int col = 1; col <= row; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
 }
