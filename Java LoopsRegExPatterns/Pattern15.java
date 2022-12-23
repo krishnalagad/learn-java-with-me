c3787a55a7e9591b574b9ca8fdca9fddf8ee154c
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-23 19:05:00 +0530

    - Pattern 15

--- Java LoopsRegExPatterns/Pattern15.java
+++ Java LoopsRegExPatterns/Pattern15.java
@@ -1,3 +1,16 @@
 public class Pattern15{
-    
+    public static void main(String[] args) {
+        pattern15(7);
+    }
+
+    private static void pattern15(int n) {
+        int i = 1;
+        for(int row = 1; row <= n; row++) {
+            for(int col = 1; col <= row; col++){
+                System.out.print(i + " ");
+                i += 1;
+            }
+            System.out.println();
+        }
+    }
 }
