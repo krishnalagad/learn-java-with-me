ee06464e8922453f8d0108b8a91ee3a33162803b
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-25 00:50:18 +0530

    -Pattern 22

--- Java LoopsRegExPatterns/Pattern22.java
+++ Java LoopsRegExPatterns/Pattern22.java
@@ -1,3 +1,21 @@
 public class Pattern22 {
-    
+    public static void main(String[] args) {
+        pattern22(5);
+    }
+
+    private static void pattern22(int n) {
+        int row, col, temp = n * 2 - 1;
+        int num = 1;
+        for (row = 1; row <= temp; row++) {
+
+            for (col = 1; col <= temp; col++) {
+                if (col == row || col == temp - row + 1) {
+                    System.out.print(num);
+                }
+                System.out.print(" ");
+            }
+            System.out.println();
+            num = (row <= temp / 2) ? ++num : --num;
+        }
+    }
 }
