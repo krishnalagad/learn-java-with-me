3cc6e5ec75a65f3b77dac3a0586e6e1ddd123aa3
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-24 23:54:07 +0530

    - Pattern 19

--- Java LoopsRegExPatterns/Pattern19.java
+++ Java LoopsRegExPatterns/Pattern19.java
@@ -1,5 +1,17 @@
 public class Pattern19 {
     public static void main(String[] args) {
-        
+        pattern19(5);
+    }
+
+    private static void pattern19(int n) {
+        for(int row = 1; row <= n; row++) {
+            for(int spaces = 1; spaces <= n - row; spaces++) {
+                System.out.print("  ");
+            }
+            for(int col = 1; col <= n; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
