078647d731090cca51e0ef93e919e3d332b4602f
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-25 00:13:52 +0530

    - Pattern 20

--- Java LoopsRegExPatterns/Pattern20.java
+++ Java LoopsRegExPatterns/Pattern20.java
@@ -1,3 +1,21 @@
 public class Pattern20 {
-    
+    public static void main(String[] args) {
+        pattern20(6);
+    }
+
+    private static void pattern20(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int spaces = 1; spaces <= n - row; spaces++) {
+                System.out.print("  ");
+            }
+            for (int col = 1; col <= n ; col++) {
+                if ((row == 1 || row == n) || (col == 1 || col == n)) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
