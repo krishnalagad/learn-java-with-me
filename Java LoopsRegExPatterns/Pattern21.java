ce488ecabbdc771e245d918f80982e56528b382f
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-25 00:21:49 +0530

    - Pattern 21

--- Java LoopsRegExPatterns/Pattern21.java
+++ Java LoopsRegExPatterns/Pattern21.java
@@ -1,3 +1,17 @@
 public class Pattern21 {
-    
+    public static void main(String[] args) {
+        pattern21(5);
+    }
+
+    private static void pattern21(int n) {
+        for(int row = 1; row <= n; row++) {
+            for(int space = 1; space <= n - row; space++) {
+                System.out.print(" ");
+            }
+            for(int col = 1; col <= row; col++) {
+                System.out.print(row + " ");
+            }
+            System.out.println();
+        }
+    }
 }
