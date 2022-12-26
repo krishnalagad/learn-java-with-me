a01182fb22dafec8db201d3a8dac005b406937b8
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-26 21:45:31 +0530

    - Pattern 23

--- Java LoopsRegExPatterns/Pattern23.java
+++ Java LoopsRegExPatterns/Pattern23.java
@@ -1,3 +1,21 @@
 public class Pattern23 {
+    public static void main(String[] args) {
+        pattern23(5);
+    }
 
+    private static void pattern23(int n) {
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
 }
