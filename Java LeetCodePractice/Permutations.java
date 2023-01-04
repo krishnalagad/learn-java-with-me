97d3905ec8bf715f34e8291c5398a89060c4bffd
Krishna Lagad <krishnalagad2@gmail.com>
2023-01-04 18:12:47 +0530

    Update Permutations.java

--- Java LeetCodePractice/Permutations.java
+++ Java LeetCodePractice/Permutations.java
@@ -1,3 +1,18 @@
 public class Permutations {
-    
+    public static void printPermutation(String str, int idx, String perm) {
+        if (str.length() == 0) {
+            System.out.println(perm);
+            return;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            char currChar = str.charAt(i);
+            String newStr = str.substring(0, i) + str.substring(i + 1);
+            printPermutation(newStr, idx + 1, perm + currChar);
+        }
+    }
+
+    public static void main(String args[]) {
+        String str = "abc";
+        printPermutation(str, 0, "");
+    }
 }
