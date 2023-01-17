92430df98c1e482ea43da070c5bb12163c22f4b8
Krishna Lagad <krishnalagad2@gmail.com>
2023-01-17 11:11:46 +0530

    - Pattern 24

--- Java LoopsRegExPatterns/Pattern24.java
+++ Java LoopsRegExPatterns/Pattern24.java
@@ -1,12 +1,12 @@
 public class Pattern24 {
     public static void main(String[] args) {
-        pattern24(5);
+        pattern24(7);
     }
 
     private static void pattern24(int n) {
-        for (int row = 0; row < n; row++) {
-            for (int col = 0; col < n; col++) {
-                if (row == 0 || row == n || col == ((row / 2) + 1)) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n; col++) {
+                if (row == 1 || row == n || col == ((n / 2) + 1)) {
                     System.out.print("* ");
                 } else {
                     System.out.print("  ");
