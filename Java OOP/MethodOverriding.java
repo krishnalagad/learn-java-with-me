40e6be55832e6cf536e9bdc1122fde4e290429f3
Krishna Lagad <krishnalagad2@gmail.com>
2023-01-17 15:29:11 +0530

    Create MethodOverriding.java

---
+++ Java OOP/MethodOverriding.java
@@ -0,0 +1,18 @@
+class Parent {
+    protected void print() {
+        System.out.println("Parent class method..");
+    }
+}
+
+class Child extends Parent {
+    public void print() {
+        System.out.println("Child class method..");
+    }
+}
+
+public class MethodOverriding {
+    public static void main(String[] args) {
+        Parent p = new Parent();
+        p.print();
+    }
+}
