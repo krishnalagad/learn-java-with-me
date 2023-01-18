a5875931c217760dd6211bb9ffceab53be9e5aee
Krishna Lagad <krishnalagad2@gmail.com>
2023-01-18 12:19:18 +0530

    - Pattern 25

--- Java LoopsRegExPatterns/Pattern25.java
+++ Java LoopsRegExPatterns/Pattern25.java
@@ -1,5 +1,34 @@
 public class Pattern25 {
     public static void main(String[] args) {
-        
+        pattern25(5);
+    }
+
+    private static void pattern25(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int spaces = 1; spaces <= n - row; spaces++) {
+                System.out.print(" ");
+            }
+            for (int col = 1; col <= row * 2 - 1; col++) {
+                if (col == 1 || col == row * 2 - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        for (int row = n - 1; row > 0; row--) {
+            for (int spaces = 1; spaces <= n - row; spaces++) {
+                System.out.print(" ");
+            }
+            for (int col = 1; col <= row * 2 - 1; col++) {
+                if (col == 1 || col == row * 2 - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
