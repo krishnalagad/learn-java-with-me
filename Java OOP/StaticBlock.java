101bf2c959d9c6a633a5e8e391996e5fac6d92da
Krishna Lagad <krishnalagad2@gmail.com>
2023-01-18 09:05:36 +0530

    Create StaticBlock.java

---
+++ Java OOP/StaticBlock.java
@@ -0,0 +1,10 @@
+public class StaticBlock {
+
+    static {
+        System.out.println("Static block code here !!");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("main method !!");
+    }
+}
