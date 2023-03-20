ffc4f12d8374cb398ee87982bbaf4203111f0e12
Krishna Lagad <krishnalagad2@gmail.com>
2023-03-20 17:01:10 +0530

    Update Code.java

--- Java OOP/Code.java
+++ Java OOP/Code.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Code{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("jk");
     }
 }
